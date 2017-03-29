4c2d973ffeb69f0fe4fb8ce7367360cd296642e0
bhavishya-singh <bhavishya.k.singh@gmail.com>
2017-03-29 23:42:06 +0530

    starting controller work

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ActiveRecord::Base
 	after_initialize :set_no_users
 	validates :name , :presence => true
-
-	has_many :users, :through => :GroupUserMapping
+	has_many :group_user_mappings
+	has_many :users, :through => :group_user_mappings
 
 	private 
 	def set_no_users
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
 	validates :user_name, :presence => true, :uniqueness => true
 
 
-
-	has_many :groups, :through => :GroupUserMapping
+	has_many :group_user_mappings
+	has_many :groups, :through => :group_user_mappings
 
 	
   # Include default devise modules. Others available are:
