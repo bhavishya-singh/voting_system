0d5693cb8648b25facfe8a7a029e70908d200094
bhavishya-singh <bhavishya.k.singh@gmail.com>
2017-03-29 19:57:54 +0530

    added user and group

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+	validates :user_name, :presence => true, :uniqueness => true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
